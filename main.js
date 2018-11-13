267bf3258f42fcedae225631771cf6cc2836a9e0
John Considine <jackconsidine3@gmail.com>
2018-11-13 15:49:59 -0600

    refactor: object destructuring to the max

--- main.js
+++ main.js
@@ -7,17 +7,17 @@
 const POST = 'Post';
 const COMMENT = 'Comment';
 
-Parse.Cloud.define('posts:get', async request => {
+Parse.Cloud.define('posts:get', async ({ params: { id } }) => {
   // Needs a post ID
-  return new Parse.Query(POST).get(request.params.id, {
+  return new Parse.Query(POST).get(id, {
     useMasterKey: true
   });
 });
 
-Parse.Cloud.define('posts:list', async request => {
+Parse.Cloud.define('posts:list', async ({ params }) => {
   // Allow pagination
-  const skip = request.params.skip || 0;
-  const limit = request.params.limit || 20;
+  const skip = params.skip || 0;
+  const limit = params.limit || 20;
 
   return new Parse.Query(POST)
     .skip(skip)
@@ -25,91 +25,57 @@ Parse.Cloud.define('posts:list', async request => {
     .find({ useMasterKey: true });
 });
 
-Parse.Cloud.define('posts:create', async request => {
-  // Post should have text and should have a user
-  if (!request.user) {
-    throw new Error('unauthenticated!');
-  }
-
-  if (!request.params.text) {
-    throw new Error('A post needs text!');
-  }
-
+Parse.Cloud.define('posts:create', async ({ user, params: { text } }) => {
   return new Parse.Object(POST, {
-    text: request.params.text,
-    user: request.user
+    text,
+    user
   }).save(null, { useMasterKey: true });
 });
 
-Parse.Cloud.define('posts:delete', async request => {
-  if (!request.user) {
-    throw new Error('unauthenticated!');
-  }
-
-  if (!request.params.text) {
-    throw new Error('A post needs text!');
-  }
-
-  if (!request.params.id) {
-    throw new Error('Need Post ID!');
-  }
-
-  const post = await new Parse.Query(POST).get(request.params.id, {
+Parse.Cloud.define('posts:delete', async ({ user, params: { id } }) => {
+  if (!user) throw new Error('Unauthenticated'); // Not necessary, but prevents unused vars
+  const post = await new Parse.Query(POST).get(id, {
     useMasterKey: true
   });
   return post.destroy({ useMasterKey: true });
 });
 
-Parse.Cloud.define('comment:get', async request => {
+Parse.Cloud.define('comment:get', async ({ params: id }) => {
   // Needs a post ID
-  return new Parse.Query(COMMENT).get(request.params.id, {
+  return new Parse.Query(COMMENT).get(id, {
     useMasterKey: true
   });
 });
 
-Parse.Cloud.define('comment:list', async request => {
+Parse.Cloud.define('comment:list', async ({ params }) => {
   // Allow pagination
-  const skip = request.params.skip || 0;
-  const limit = request.params.limit || 20;
+  const skip = params.skip || 0;
+  const limit = params.limit || 20;
 
   return new Parse.Query(COMMENT).skip(skip).limit(limit);
 });
 
-Parse.Cloud.define('comment:create', async request => {
-  // Post should have text and should have a user and a post id
-  if (!request.user) {
-    throw new Error('unauthenticated!');
+Parse.Cloud.define(
+  'comment:create',
+  async ({ user, params: { post_id, text } }) => {
+    // Post should have text and should have a user and a post id
+    if (!user) throw new Error('Unauthenticated'); // Not necessary, but prevents unused vars
+
+    const post = await new Parse.Query(POST).get(post_id, {
+      useMasterKey: true
+    });
+    return new Parse.Object(COMMENT, {
+      text,
+      user,
+      post
+    }).save(null, { useMasterKey: true });
   }
+);
 
-  if (!request.params.text) {
-    throw new Error('A comment needs text!');
-  }
-  if (!request.params.post_id) {
-    throw new Error('A comment needs a post!');
-  }
+Parse.Cloud.define('comment:delete', async ({ user, params: { id } }) => {
+  if (!user) throw new Error('Unauthenticated'); // Not necessary, but prevents unused vars
 
-  //   Get the post
-
-  const post = await new Parse.Query(POST).get(request.params.post_id, {
-    useMasterKey: true
-  });
-  return new Parse.Object(COMMENT, {
-    text: request.params.text,
-    user: request.user,
-    post: post
-  }).save(null, { useMasterKey: true });
-});
-
-Parse.Cloud.define('comment:delete', async request => {
-  if (!request.user) {
-    throw new Error('unauthenticated!');
-  }
-
-  if (!request.params.id) {
-    throw new Error('Need Comment ID!');
-  }
-
-  const comment = await new Parse.Query(COMMENT).get(request.params.id, {
+  const comment = await new Parse.Query(COMMENT).get(id, {
     useMasterKey: true
   });
 
