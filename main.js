b013f1c1ea5894d9415df46d8897e6d2f04be1da
John Considine <jackconsidine3@gmail.com>
2018-11-13 16:22:34 -0600

    refactor: assert parameters

--- main.js
+++ main.js
@@ -7,7 +7,14 @@
 const POST = 'Post';
 const COMMENT = 'Comment';
 
+const AssertParams = parameter_obj => {
+  for (var key of Object.keys(parameter_obj)) {
+    if (!parameter_obj[key]) throw new Error(`Missing parameter ${key}`);
+  }
+};
+
 Parse.Cloud.define('posts:get', async ({ params: { id } }) => {
+  AssertParams({ id });
   // Needs a post ID
   return new Parse.Query(POST).get(id, {
     useMasterKey: true
@@ -26,6 +33,7 @@ Parse.Cloud.define('posts:list', async ({ params }) => {
 });
 
 Parse.Cloud.define('posts:create', async ({ user, params: { text } }) => {
+  AssertParams({ user, text });
   return new Parse.Object(POST, {
     text,
     user
@@ -33,7 +41,7 @@ Parse.Cloud.define('posts:create', async ({ user, params: { text } }) => {
 });
 
 Parse.Cloud.define('posts:delete', async ({ user, params: { id } }) => {
-  if (!user) throw new Error('Unauthenticated'); // Not necessary, but prevents unused vars
+  AssertParams({ user, id });
   const post = await new Parse.Query(POST).get(id, {
     useMasterKey: true
   });
@@ -41,7 +49,7 @@ Parse.Cloud.define('posts:delete', async ({ user, params: { id } }) => {
 });
 
 Parse.Cloud.define('comment:get', async ({ params: id }) => {
-  // Needs a post ID
+  // Don't need to assert parameter necessarily because 'get' will error without an id
   return new Parse.Query(COMMENT).get(id, {
     useMasterKey: true
   });
@@ -58,8 +66,8 @@ Parse.Cloud.define('comment:list', async ({ params }) => {
 Parse.Cloud.define(
   'comment:create',
   async ({ user, params: { post_id, text } }) => {
+    AssertParams({ post_id, text, user });
     // Post should have text and should have a user and a post id
-    if (!user) throw new Error('Unauthenticated'); // Not necessary, but prevents unused vars
 
     const post = await new Parse.Query(POST).get(post_id, {
       useMasterKey: true
@@ -73,8 +81,7 @@ Parse.Cloud.define(
 );
 
 Parse.Cloud.define('comment:delete', async ({ user, params: { id } }) => {
-  if (!user) throw new Error('Unauthenticated'); // Not necessary, but prevents unused vars
-
+  AssertParams({ id, user });
   const comment = await new Parse.Query(COMMENT).get(id, {
     useMasterKey: true
   });
